de576c6927ca6661836e966d5ae27b47b50809b3
Daniel Vázquez <danielvazquezmunoz@gmail.com>
2023-08-22 17:21:24 +0200

    ?

--- src/main/java/wtf/casper/lightdupefix/LightDupeFix.java
+++ src/main/java/wtf/casper/lightdupefix/LightDupeFix.java
@@ -1,12 +1,8 @@
 package wtf.casper.lightdupefix;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,22 +12,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public final class LightDupeFix extends JavaPlugin implements Listener {
 
-    private final Cache<Location, Byte> blockCache = CacheBuilder.newBuilder()
-            .expireAfterWrite(3, TimeUnit.SECONDS)
-            .build();
-
-    private final BlockFace[] around = new BlockFace[]{
-            BlockFace.NORTH,
-            BlockFace.EAST,
-            BlockFace.SOUTH,
-            BlockFace.WEST
-    };
-
     private final Set<Material> scannedMaterials = new HashSet<>();
+    private final Set<Block> blocks = new HashSet<>();
 
     @Override
     public void onEnable() {
@@ -69,39 +54,26 @@ public final class LightDupeFix extends JavaPlugin implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getItem() == null) {
-            return;
-        }
-        if (event.getClickedBlock() == null) {
-            return;
-        }
-        if (!scannedMaterials.contains(event.getItem().getType())) {
-            return;
+        final Block block = event.getClickedBlock();
+        if (event.getItem() == null || block == null) return;
+
+        if (!scannedMaterials.contains(event.getItem().getType())) return;
+
+        Block downBlock = block.getRelative(BlockFace.DOWN);
+
+        if (downBlock.getType().equals(Material.FARMLAND) && downBlock.getLightLevel() == 0) {
+            blocks.add(block);
         }
 
-        final Block block = event.getClickedBlock().getRelative(event.getBlockFace());
-
-        for (BlockFace face : around) {
-            Block relative = block.getRelative(face);
-            if (isInvalidCrop(relative)) {
-                blockCache.put(relative.getLocation(), (byte) 0);
-            }
-        }
     }
 
     @EventHandler
     public void onPhysicsEvent(BlockPhysicsEvent event) {
-        if (blockCache.getIfPresent(event.getBlock().getLocation()) != null) {
+        final Block block = event.getBlock();
+        if (blocks.contains(block)) {
             event.setCancelled(true);
-            blockCache.invalidate(event.getBlock().getLocation());
+            blocks.remove(block);
         }
     }
 
-    private boolean isInvalidCrop(Block block) {
-        if (!(block.getBlockData() instanceof Ageable)) {
-            return false;
-        }
-        return !(block.getRelative(BlockFace.DOWN).getType() == Material.FARMLAND
-                && block.getRelative(BlockFace.UP).getLightLevel() >= 7);
-    }
 }
